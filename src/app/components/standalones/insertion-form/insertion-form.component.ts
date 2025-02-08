97be76b68e9d690e627b56bff71c449deeb7a177
cfl.dev.apps <cfl.dev.apps@gmail.com>
2025-02-08 15:02:05 +0000

    updating filed validation

--- src/app/components/standalones/insertion-form/insertion-form.component.ts
+++ src/app/components/standalones/insertion-form/insertion-form.component.ts
@@ -1,23 +1,28 @@
 import { DepartmentService } from './../../../services/department/department.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { Record } from '../../../models/record';
-import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { AbstractControl, FormsModule, ValidationErrors } from '@angular/forms';
+import { NgClass, NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Department } from '../../../models/department';
 import { RecordService } from '../../../services/record/record.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { parse, isValid } from 'date-fns';
+import { delay, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-insertion-form',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule, NgFor, NgIf, UpperCasePipe, NgxMaskDirective],
+  imports: [ReactiveFormsModule, FormsModule, NgFor, NgIf, NgClass, UpperCasePipe, NgxMaskDirective],
   providers:[provideNgxMask()],
   templateUrl: './insertion-form.component.html',
   styleUrl: './insertion-form.component.css'
 })
 export class InsertionFormComponent implements OnInit{
+
+  currentDate = new Date().toLocaleDateString('en-UK')
+  alertSuccess = false;
 
   @Input() recordType!: number;
   @Input() expenseType!: string;
@@ -27,13 +32,9 @@ export class InsertionFormComponent implements OnInit{
   
   recordForm: FormGroup;
   
+
   constructor(private recordService: RecordService, private departmentService: DepartmentService, private fb: FormBuilder){ 
-    this.recordForm = this.fb.group({
-      descricao: ['', [Validators.required, Validators.minLength(3)]], // Campo Descrição
-      amount: ['', [Validators.required]], // Campo Amount
-      date: ['', Validators.required], // Campo Date
-      departmentId: [1, [Validators.required, Validators.min(1)]], // Campo DepartmentId
-    })
+   this.recordForm = this.startForm()
   }
 
   ngOnInit(): void {
@@ -50,14 +51,37 @@ export class InsertionFormComponent implements OnInit{
         this.recordType,
         this.expenseType
       )
-      console.log(record)
+      this.recordService.save(record)
+      this.showAlert()
     }
 
   }
+  
+  showAlert(){
+    this.alertSuccess = true
+    setTimeout(() => {
+      this.alertSuccess = false;
+    }, 5000);
+  }
 
-  onInput(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    input.value = input.value.replace(/[^0-9,.]/g, ''); // Substitui qualquer coisa que não seja número
+
+  dateFormatValidator(control: AbstractControl): Observable<ValidationErrors | null>{
+    const date = parse(control.value, 'dd/MM/yyyy', new Date());
+
+  return of(isValid(date) ? null : { invalidDate: true }).pipe(delay(500));
+  }
+
+  clean(){
+    this.recordForm = this.startForm()
+  }
+
+  startForm(){
+    return this.fb.group({
+      descricao: ['', [Validators.required, Validators.minLength(3)]], // Campo Descrição
+      amount: ['', [Validators.required]], // Campo Amount
+      date: ['', Validators.required, this.dateFormatValidator], // Campo Date
+      departmentId: [1, [Validators.required, Validators.min(1)]], // Campo DepartmentId
+    })
   }
 
 }
